a709ebf55d1bda093554dfd0fe387ab4c6b190a4
kinyanjuiprince21 <kinyanjuiprince21@gmail.com>
2024-01-12 21:46:30 +0300

    Initial commit

--- src/contact.jsx
+++ src/contact.jsx
@@ -22,12 +22,20 @@ function Book(){
     const serviceId = 'service_pk6ns6r'
     const templateId = 'template_0894n1p'
     const userId = "TwwaTtvgHlr30ezwU"
-    const form = useRef(null)
+    const form = useRef(null);
+    const userName = useRef(null);
+    const userEmail = useRef(null);
+    const userMessage = useRef(null);
 
+    const templateParams = {
+        name: userName,
+        email: userEmail,
+        message: userMessage,
+    }
     const handleFormSubmit = (e) => {
         e.preventDefault();
 
-        emailjs.sendForm(serviceId, templateId, form.current, userId)
+        emailjs.sendForm(serviceId, templateId, templateParams, userId)
             .then((result) => {
                 console.log(result.text);
                 swal.fire(
@@ -65,16 +73,16 @@ function Book(){
                     <div className="top">
                         <div className="input">
                             <label htmlFor="fullname">Full Name</label>
-                            <input type="text" name="user-name" id="" placeholder="Enter full name"/>
+                            <input type="text" name="user_name" id="user_name" placeholder="Enter full name" ref={userName}/>
                         </div>
                         <div className="input">
                             <label htmlFor="email">Email</label>
-                            <input type="email" name="user-email" id="" placeholder="Enter your email"/>
+                            <input type="email" name="user_email" id="user_email" placeholder="Enter your email" ref={userEmail}/>
                         </div>
                     </div>
                     <div className="input">
                         <label htmlFor="message">Message</label>
-                        <textarea name="user-message" id="" cols="30" rows="10" placeholder="Write a message for me."></textarea>
+                        <textarea name="user_message" id="user_message" cols="30" rows="10" placeholder="Write a message for me." ref={userMessage}></textarea>
                     </div>
                     <input type="submit" value="SEND MESSAGE" />
                 </form>
